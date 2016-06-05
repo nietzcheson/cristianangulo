12ee5a0f51efbbe232836dadd7ae13f7ffb20e31
Cristian Angulo Nova <cristianangulonova@gmail.com>
2016-06-05 12:32:55 -0500

    Modify

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,37 @@
 'use strict';
 
 var gulp = require('gulp'),
-     sass = require('gulp-sass'),
-     browserSync = require('browser-sync'),
-     reload = browserSync.reload;
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync'),
+    reload = browserSync.reload,
+    shell = require('gulp-shell'),
+    exec = require('gulp-exec');
 
-     gulp.task('browser-sync', function() {
-       //watch files
-       var files = [
-         'content/css/app.css',
-         //'js/**/*.js',
-         //'images/**/*',
-         'content/**/*.html',
-         'includes/**/*.html',
-         'layouts/**/*.html',
-       ];
-       //initialize browsersync
-       browserSync.init(files, {
-         proxy: "http://localhost:4000/" // BrowserSync proxy, change to match your local environment
-       });
-     });
+    gulp.task('browser-sync', function() {
+        var files = [
+            'src/content/css/*.sass',
+            'src/content/**/*.html',
+            'src/includes/**/*.html',
+            'src/layouts/**/*.html',
+        ];
 
-gulp.task('default', ['browser-sync'], function(){
-});
+        browserSync.init(files, {
+            proxy: "http://localhost:4000/" // BrowserSync proxy, change to match your local environment
+        });
+
+        gulp.watch("src/content/css/*.sass", ['sass']);
+    });
+
+    var input = 'src/content/css/*.sass';
+    var output = 'src/content/css';
+
+    gulp.task('sass', function(){
+        return gulp
+            .src(input)
+
+            .pipe(sass())
+
+            .pipe(gulp.dest(output));
+    });
+
+    gulp.task('default', ['browser-sync'], function(){});
